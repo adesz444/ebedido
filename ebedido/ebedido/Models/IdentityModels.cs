e63ea038e11a3003e473c38058f68905015c3c43
Szabo Adam <adesz444@gmail.com>
2016-01-25 00:10:23 +0100

    contact

--- ebedido/ebedido/Models/IdentityModels.cs
+++ ebedido/ebedido/Models/IdentityModels.cs
@@ -29,5 +29,7 @@ namespace ebedido.Models
         {
             return new ApplicationDbContext();
         }
+
+        public System.Data.Entity.DbSet<ContactManager.Models.Contact> Contacts { get; set; }
     }
 }
